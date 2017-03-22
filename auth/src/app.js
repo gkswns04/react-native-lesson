5a780431208992ccce61da0be3533bf86792e50f
gkswns04 <gkswns04@naver.com>
2017-03-22 15:22:55 +0900

    ADD(all)

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyARhOaBU6sYsFM_S0P6EFyhoqDA-qXi1vI',
+      authDomain: 'authentication-9b945.firebaseapp.com',
+      databaseURL: 'https://authentication-9b945.firebaseio.com',
+      storageBucket: 'authentication-9b945.appspot.com',
+      messagingSenderId: '540538124337'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
